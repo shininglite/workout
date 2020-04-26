814ac8114b13b3aa84d1155bb4a1046ab5230cc2
shininglite <vandeusent@gmail.com>
2020-04-26 17:35:16 -0500

    post route for exercise working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
-
+var path = require("path");
 const app = express();
 // dev is for development environment in morgan
 app.use(logger("dev"));
@@ -17,26 +17,37 @@ app.use(express.static("public"));
 // name of database goes after localhost
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/", (req, res) => {
-  res.send("root route working");
-});
-
+// get route to find all workouts
 app.get("/api/workouts", (req, res) => {
-  
   db.Workout.find({})
   .then(dbWorkout => {
+    console.log(dbWorkout);
     res.json(dbWorkout);
-    res.send("workouts working");
+    
+    // res.send("api workouts working");// this works in postman with 3000/api/workouts
   })
   .catch(err => {
+    res.status(404).send('Sorry, cant find that');
     res.json(err);
   });
-
 });
 
+//create new exercise
+app.post("/api/workouts", ({body}, res) => {
+  db.Workout.create(body)
+  // id of the new exercise we just created, gets pushed in to the workout field into the inside the workout it is associated with 
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// post route to update an existing exercise
 app.post("/exercise/:id", ({body}, res) => {
   db.Workout.create(body)
-  // id of the new exercise we just created, gets pushed in to the workout field into the inside the ________ it is associated with 
+  // id of the new exercise we just created, gets pushed in to the workout field into the inside the workout it is associated with 
     .then(({_id}) => db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { workout: _id } }, { type: "Benchpress" }))
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -47,15 +58,32 @@ app.post("/exercise/:id", ({body}, res) => {
 });
 
 app.get("/api/workouts/range", (req, res) => {
-  res.send("workouts range working");
+  res.send("workouts range working"); // this works in postman with 3000/api/workouts/range
 });
 
-// app.get("/exercise", (req, res) => {
-//   res.send("exercise route working");
-// });
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+// post route to add a new exercise
+app.post("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/exercise.html"));
+  //res.send("exercise route working");
+});
+
+//app.get("/tables", function(req, res) {
+//  res.sendFile(path.join(__dirname, "../public/tables.html"));
+//});
 
 app.get("/stats", (req, res) => {
-  res.send("stats route working");
+  
+  // res.send("stats route working");
+});
+
+// catch all route
+app.get("/", (req, res) => {
+  //res.sendFile(path.join(__dirname + "./public/index.html"));
+  res.send("root route working");
 });
 
 app.listen(PORT, () => {
